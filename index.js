de5aa2ec0aa9afa0156996cd9a460bbd2d8025aa
Kamil Dąbrowski <kamildabrowski@iMac-Kamil.local>
2018-11-16 00:53:49 +0100

    corrections

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
-require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
